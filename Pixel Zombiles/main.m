b052deed83d3965cd2227363e513d2a59691d9e1
Brandon Trebitowski <brandontreb@gmaill.com>
2012-09-11 19:44:31 -0600

    Initial Commit

---
+++ Pixel Zombiles/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pixel Zombiles
+//
+//  Created by Brandon Trebitowski on 9/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
